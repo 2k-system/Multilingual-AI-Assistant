928b47624e75eab8bb03fe8530de16cf419b8d84
System......... <84314668+2k-system@users.noreply.github.com>
2026-04-17 23:34:51 +0530

    Update package name and author email in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="multilingual assistant",
+    name="Multilingual AI Assistant",
     version="0.0.0",
     author="sys",
-    author_email="abcd.ai",
+    author_email="pranalidhawade@gmail.com",
     packages=find_packages(),
     install_requires=["SpeechRecognition","pipwin","pyaudio","gTTS","google-generativeai","python-dotenv","streamlit"]
 )
